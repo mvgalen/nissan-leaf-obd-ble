c7e962a65a161ac08cd616d19a3309a2539c3026
mvgalen <marijnvangalen@gmail.com>
2024-11-13 16:02:47 +0100

    without \r

--- custom_components/nissan_leaf_obd_ble/api.py
+++ custom_components/nissan_leaf_obd_ble/api.py
@@ -41,7 +41,7 @@ class NissanLeafObdBleApiClient:
         data = {}
         for command in leaf_commands.values():
             if command.name == "lbc":
-                lbc_cmd = OBDCommand("lbc",                   "Li-ion battery controller",    b"02210100000000000\r \r",      0, lbc, header=b"79B",)
+                lbc_cmd = OBDCommand("lbc",                   "Li-ion battery controller",    b"02210100000000000",      0, lbc, header=b"79B",)
                 await api.send(lbc_cmd, force=True)
             response = await api.query(command, force=True)
             # the first command is the Mystery command. If this doesn't have a response, then none of the other will
