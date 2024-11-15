84cf93f61c1851bff4947b4042d1917d85aa6d90
mvgalen <marijnvangalen@gmail.com>
2024-11-15 20:06:08 +0100

    test

--- custom_components/nissan_leaf_obd_ble/api.py
+++ custom_components/nissan_leaf_obd_ble/api.py
@@ -39,15 +39,15 @@ class NissanLeafObdBleApiClient:
             return None
 
         data = {}
-        w1_cmd = OBDCommand("wake1",                   "WAKE1",    b"00 0",      0, lbc, header=b"682",fast=False)
+        w1_cmd = OBDCommand("wake1",                   "WAKE1",    b"0100",      0, lbc, header=b"682",fast=False)
         await api.query(w1_cmd, force=True)
-        w2_cmd = OBDCommand("wake2",                   "WAKE2",    b"00",      0, lbc, header=b"603",fast=True)
+        w2_cmd = OBDCommand("wake2",                   "WAKE2",    b"0100 0",      0, lbc, header=b"603",fast=False)
         await api.query(w2_cmd, force=True)
-        w3_cmd = OBDCommand("wake3",                   "WAKE3",    b"00",      0, lbc, header=b"5c0",fast=True)
+        w3_cmd = OBDCommand("wake3",                   "WAKE3",    b"0100",      0, lbc, header=b"5c0",fast=False)
         await api.query(w3_cmd, force=True)
-        w4_cmd = OBDCommand("wake4",                   "WAKE4",    b"00",      0, lbc, header=b"679",fast=True)
+        w4_cmd = OBDCommand("wake4",                   "WAKE4",    b"0100",      0, lbc, header=b"679",fast=False)
         await api.query(w4_cmd, force=True)
-        await api.query(w1_cmd, force=True)
+#        await api.query(w1_cmd, force=True)
 
 #        cra = OBDCommand("cra",                   "CRA",    b"CRA 5b3 1",      0, lbc, header=b"5b3",)
 #        response = await api.rawquery(cra, force=True)
